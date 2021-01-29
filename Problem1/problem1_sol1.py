d1d8599adfe503aec8fd7de8592a6ba40c0e5b2b
Droogy <drooogy@pm.me>
2021-01-29 11:34:45 -0500

    added problem

--- Problem1/problem1_sol1.py
+++ Problem1/problem1_sol1.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 
-#Scenario
-#---------
-#I forgot a WinRAR-archive password.
-#I remember there was a name of someone from my (hypothetical) family: jake, melissa, oliver, emily.
-#I remember there was a year of birth of someone from my family: 1987, 1954, 1963.
-#I remember there was a symbol (that are so recommended by everyone):
-#["!","@","#","$","%","&","*","-","=","_","+",".",","]
-#But I do not remember an order of these elements.
-#You have to find a list of all possible passwords, based on this information.
-#There are ~936 of them.
-
 names = ['jake', 'melissa', 'oliver', 'emily']
 years = ['1987', '1954', '1963']
 symbols = ["!","@","#","$","%","&","*","-","=","_","+",".",","]
